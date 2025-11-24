92440af68422d684b56ada13947a6066bbce9614
candylyt <candyliu.lyt@gmail.com>
2025-11-23 22:24:30 -0500

    update database connection

--- database.py
+++ database.py
@@ -9,7 +9,16 @@ DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
+INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME") 
 
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+unix_socket_path = f"/cloudsql/{INSTANCE_CONNECTION_NAME}"
+
+# DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+DATABASE_URL = (
+    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}"
+    f"?unix_socket={unix_socket_path}"
+)
+
 
 engine = create_engine(DATABASE_URL, echo=False)
